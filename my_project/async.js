b309b10f7e08acd36b6d6c52ad99af8bc7e88212
zhangyy01 <zhangyy01@fenbi.com>
2020-08-04 20:11:18 +0800

    not

--- my_project/async.js
+++ my_project/async.js
@@ -1,17 +1,30 @@
 const fs = require('fs')
 
 // module.exports = async () => {
-//     let expression = await fs.readFile('./data.txt','utf-8')
+//     let expression = await fs.readFile('./data.txt','utf-8',(err,data) =>{
+//         console.log('done')
+//     })
 //     let fn = new Function('return' + expression)
 //     let r = fn()
 //     console.log(`Calculate: ${expression} = ${r}`)
 //     return r
 // }
 
-module.exports = async () => {
-    let expression = await fs.readFile('./data.txt','utf-8')
-    let fn = new Function('return' + expression)
+fun = async () => {
+    let expression = await fs.readFile('./data.txt','utf-8',(err,data) => {
+        if (err) {
+            console.log(err)
+        } else {
+            return data
+        }
+    })
+    console.log('expression:',expression)
+    let fn = new Function( 'return ' +expression)
+    console.log('fn:',fn)
     let r = fn()
+    console.log('r:',r)
     console.log(`Calculate: ${expression} = ${r}`)
     return r
 }
+
+fun()
